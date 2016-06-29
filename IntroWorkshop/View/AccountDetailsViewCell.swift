22939fad7f9d8df67a45050d0a43c7215171cc98
Kenneth Ko <Ken.Ko@cba.com.au>
2016-06-30 09:34:45 +1000

    Session 4.1 - Converting accounts view to dynamic TableView

---
+++ IntroWorkshop/View/AccountDetailsViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AccountDetailsViewCell.swift
+//  IntroWorkshop
+//
+//  Created by Ken Ko on 30/06/2016.
+//  Copyright © 2016 Commonwealth Bank of Australia. All rights reserved.
+//
+
+import UIKit
+
+class AccountDetailsViewCell: UITableViewCell {
+    @IBOutlet weak var accountDetailsView: AccountDetailsView!
+}
